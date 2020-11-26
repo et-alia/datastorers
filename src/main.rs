55dc8de1adcc7882fb555424afdd8819b94047d8
Andreas Pålsson <a.palsson@gmail.com>
2020-11-26 20:26:15 +0100

    Update example code to make it work again after macro refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,99 @@
-extern crate hyper;
-extern crate hyper_rustls;
-extern crate yup_oauth2 as oauth2;
-extern crate google_datastore1 as datastore1;
+use datastore_entity::{DatastoreEntity, DatastoreProperties, DatastoreManaged};
+use google_datastore1::Client;
+use google_datastore1::schemas::{Key, PathElement, LookupRequest, LookupResponse};
 
-extern crate datastore_entity;
-use datastore_entity::DatastoreEntity;
+use gcp_auth::{Error as GCPAuthError, Token};
+use google_api_auth::GetAccessToken;
 
-
-use datastore1::LookupRequest;
-use datastore1::Key;
-use datastore1::PathElement;
-
-use std::default::Default;
-use datastore1::Datastore;
-use yup_oauth2 as oauth;
+use std::convert::TryInto;
+use std::error::Error;
 
 // TODO - move from here to an integation test + make em configurable
 static TEST_KEY: &str = "./test-key.json";
 static TEST_PROJECT_NAME: &str = "hugop-238317";
 
-pub trait DatastoreEntity<T> {
 
-    fn from_result_map(hm: &std::collections::HashMap<String, datastore1::Value>) -> T;
+#[derive(DatastoreManaged, Debug)]
+#[kind = "Game"]
+struct Game {
+    #[key]
+    pub key: Option<Key>,
 
-}
-
-pub trait Datastorers<T> {
-
-    fn from_lookup_response(response: &datastore1::LookupResponse) -> T;
-
-    fn lookup(id: &String) -> Option<T>;
-
+    pub Name: String, 
+    pub Bool: bool,
 }
 
 
-#[derive(DatastoreEntity)]
 #[derive(Debug)]
-#[derive(Default)]
-struct Game { // TODO - this is is just an entity used for testing - change trait to beeing a lib and move this to some integration test
-    Name: String, // <- TODO: The names in the struct maps one to one to the property names in datastore
-    Value: i32,   // But the uppercase names generates warnings <- change how this works?
-    Bool: bool,
+pub(crate) struct GoogleToken(pub Token);
+
+impl GetAccessToken for GoogleToken {
+    fn access_token(&self) -> Result<String, Box<dyn Error + Send + Sync>> {
+        Ok(self.0.as_str().to_string())
+    }
 }
 
-
-impl Datastorers<Game> for Game { // TODO - Change so this fellow gets implemented for all structs implementinf the DatastoreEntity trait - that is possible?
-    fn from_lookup_response(response: &datastore1::LookupResponse) -> Game {
-        // TODO - many unwrap without error checking - go add it
-        Game::from_result_map(response.found.as_ref().unwrap().get(0).unwrap().entity.as_ref().unwrap().properties.as_ref().unwrap())
+impl GoogleToken {
+    #[tokio::main]
+    pub(crate) async fn get_token() -> Result<Self, GCPAuthError> {
+        let authentication_manager = gcp_auth::init().await?;
+        let token = authentication_manager
+            .get_token(&[
+                "https://www.googleapis.com/auth/datastore",
+            ])
+            .await?;
+        Ok(Self(token))
     }
+}
 
-    fn lookup(id: &String) -> Option<Game> { // TODO - Create some trait/struct representing a connection, let it contain the hub and project name
-        let client_secret = oauth::service_account_key_from_file(&String::from(TEST_KEY)).unwrap(); // TODO - handle error - not just unwrap
-        let access = yup_oauth2::ServiceAccountAccess::new(
-            client_secret,
-            hyper::Client::with_connector(hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new()))
-        );
-        let hub = Datastore::new(
-            hyper::Client::with_connector(hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new())),
-            access
-        );
+fn test_get_entity() -> Result<Game, String> {
 
-        let req = LookupRequest {
-            keys: Some(vec!(Key {
-                path: Some(vec!(PathElement {
-                    kind: Some("Game".to_string()), // TODO - game is the name if the entity - add method on DatastoreEntity that can return the name?
-                    id: Some(id.to_string()),
-                    ..Default::default()
-                })),
-                ..Default::default()
-            })),
-            ..Default::default()
-        };
+    let gtoken = GoogleToken::get_token()
+        .map_err(|_e: gcp_auth::Error| -> String {"Failed to fetch entity".to_string()})?;
+    let client = Client::new(gtoken);
 
-        let result = hub
-            .projects()
-            .lookup(req, TEST_PROJECT_NAME)
-            .doit();
+    let projects = client.projects();
 
-        match result {
-            Err(_e) => { // TODO - do something with this error?
-                None
-            },
-            Ok(res) => {
-                let ret = Game::from_lookup_response(&res.1);
-                Some(ret)
-            }
-        }
+    let key = Key{
+        partition_id: None,
+        path: Some(vec![PathElement {
+            id: Some(5632499082330112),
+            kind: Some("Game".to_string()),
+            name: None,
+        }]),
+    };
+    let req = LookupRequest{
+        keys: Some(vec![key]),
+        read_options: None
+    };
+    let resp: LookupResponse = projects.lookup(req, TEST_PROJECT_NAME).execute()
+        .map_err(|_e: google_datastore1::Error| -> String {"Failed to fetch entity".to_string()})?;
     
+    match resp.found {
+        Some(found) => {
+            
+            for f in found {
+                if let Some(entity) = f.entity {
+                    let props = DatastoreProperties::from_map(entity.properties.unwrap());
+                    let game: Game = DatastoreEntity::from(entity.key, props).try_into().unwrap();
+                    return Ok(game);
+                }
+            }
+            Err("No matching entity found".to_string())
+        },
+        None => Err("No matching entity found".to_string())
     }
+
 }
 
 fn main() {
 
-    let game = Game::lookup(&String::from("5632499082330112")); // Testing!
-    println!("Success: {:?}", game); // Validate result + move this to an integration test
-    
+    match test_get_entity() {
+        Ok(game) => {
+            println!("Success: {:#?}", game);
+        },
+        Err(err) => {
+            println!("Error fetching data {:?}", err);
+        }
+    }    
 }
